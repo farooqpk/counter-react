42695e1a9cba4bf881c44b84bfeac4ec9daee9bf
Ummar Farooq <farooqcherur@gmail.com>
2023-02-01 12:58:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { useState } from 'react'
+
 function App() {
+
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="App d-flex flex-column align-items-center justify-content-center" style={{ height: "100vh" }}>
+
+      <div className='mb-5'>
+        <h1>Counter</h1>
+      </div>
+
+  <div className="card border-dark bg-light text-center mb-4 w-50">
+    <div className="card-body text-primary">{count}</div>
+  </div>
+
+  <div className="d-flex ">
+    <button className="btn btn-outline-success m-2 btn-equal" onClick={() => { setCount(count + 1) }}>+</button>
+    <button className="btn btn-outline-warning m-2 btn-equal" onClick={() => { setCount(count > 0 ? count - 1 : 0) }}>-</button>
+    <button className="btn btn-outline-danger m-2 btn-equal" onClick={() => { setCount(0) }}>reset</button>
+  </div>
+
+</div>
+  
+  )
 }
 
+
+
+
+
 export default App;
+
+
